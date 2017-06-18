448e589f155c7028064f515606a1e9ce0b3e629b
kepo44ka <taisia.sycheva@gmail.com>
2017-06-18 23:43:45 +0400

    v0.1

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Get_Feed = new System.Windows.Forms.Button();
+            this.Save_Text = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Parse = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Get_Feed
+            // 
+            this.Get_Feed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Get_Feed.Location = new System.Drawing.Point(12, 236);
+            this.Get_Feed.Name = "Get_Feed";
+            this.Get_Feed.Size = new System.Drawing.Size(105, 23);
+            this.Get_Feed.TabIndex = 0;
+            this.Get_Feed.Text = "Get Original Feed";
+            this.Get_Feed.UseVisualStyleBackColor = true;
+            this.Get_Feed.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Save_Text
+            // 
+            this.Save_Text.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Save_Text.Location = new System.Drawing.Point(342, 236);
+            this.Save_Text.Name = "Save_Text";
+            this.Save_Text.Size = new System.Drawing.Size(105, 23);
+            this.Save_Text.TabIndex = 1;
+            this.Save_Text.Text = "Save Parsed Feed";
+            this.Save_Text.UseVisualStyleBackColor = true;
+            this.Save_Text.Click += new System.EventHandler(this.Save_Text_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(1, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(1);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(461, 232);
+            this.splitContainer1.SplitterDistance = 177;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(1);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(172, 231);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.AcceptsTab = true;
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(0, 0);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(1);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(276, 228);
+            this.textBox2.TabIndex = 1;
+            // 
+            // Parse
+            // 
+            this.Parse.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Parse.Location = new System.Drawing.Point(178, 236);
+            this.Parse.Name = "Parse";
+            this.Parse.Size = new System.Drawing.Size(105, 23);
+            this.Parse.TabIndex = 3;
+            this.Parse.Text = "Parse";
+            this.Parse.UseVisualStyleBackColor = true;
+            this.Parse.Click += new System.EventHandler(this.Parse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 262);
+            this.Controls.Add(this.Parse);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.Save_Text);
+            this.Controls.Add(this.Get_Feed);
+            this.MinimumSize = new System.Drawing.Size(475, 300);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Get_Feed;
+        private System.Windows.Forms.Button Save_Text;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Parse;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] feed = new string[101];
+            if (File.Exists("feed.txt"))
+            {
+                feed = File.ReadAllLines("feed.txt");
+                this.textBox1.Lines = feed;
+            }
+        }
+
+        private void Parse_Click(object sender, EventArgs e)
+        {
+            char separator = ':';
+            string[] feed = this.textBox1.Lines;
+            string[] parcedData = new string[150];
+            int[] dataStringNumber = new int[101];
+            string[] format = {	    //array to help with recognition of the data types
+				"date", 		            //have 101 strings, to present each string in file feed.txt
+				"float",	                //reg1
+				"float2p",	            //float 2nd part
+				"float",
+                "float2p",
+                "float",
+                "float2p",
+                "float",
+                "float2p",
+                "long",
+                "long2p",
+                "float",	                //reg11
+				"float2p",
+                "long",
+                "long2p",
+                "float",
+                "float2p",
+                "long",
+                "long2p",
+                "float",
+                "float2p",
+                "long",	                //reg21
+				"long2p",
+                "float",
+                "float2p",
+                "long",
+                "long2p",
+                "float",
+                "float2p",
+                "long",
+                "long2p",
+                "float",	                //reg31
+				"float2p",
+                "float",
+                "float2p",
+                "float",
+                "float2p",
+                "float",
+                "float2p",
+                "float",
+                "float2p",
+                "float",	                //reg41
+				"float2p",
+                "float",
+                "float2p",
+                "float",
+                "float2p",
+                "float",
+                "float2p",
+                "bcd1p",
+                "bcd2p",
+                "bcd",	                //reg51
+				"none",
+                "bcd1pof3",	        //BCD 1st part of 3
+				"bcd2pof3",
+                "bcd3pof3",
+                "bcd",	                //reg56
+				"none",
+                "none",
+                "int",	                    //reg59
+				"int",
+                "int",	                    //reg61
+				"int",
+                "int",
+                "none",
+                "none",
+                "none",
+                "none",
+                "none",
+                "none",
+                "none",
+                "none",	                //reg71
+				"bit",
+                "none",
+                "none",
+                "none",
+                "none",
+                "float",
+                "float2p",
+                "float",
+                "float2p",
+                "float",	                //reg81
+				"float2p",
+                "float",
+                "float2p",
+                "float",
+                "float2p",
+                "float",
+                "float2p",
+                "float",
+                "float2p",
+                "none",	                //reg91
+				"int_reg92",
+                "int_rng2048",
+                "int_rng2048",
+                "none",
+                "int",
+                "float",
+                "float2p",
+                "float",
+                "float2p"
+             };
+
+            //feed = this.textBox1.Lines;
+            textBox2.AppendText("Date:" + feed[0] + "\r\n");
+            for (int i = 1; i < feed.Length; ++i)
+            {
+                
+                int indexOfSeparator = feed[i].IndexOf(separator);
+                dataStringNumber[i] = Convert.ToInt32(feed[i].Substring(0, indexOfSeparator));
+                feed[i] = feed[i].Substring(indexOfSeparator + 1);
+
+                switch (format[i])
+                {
+                    case "float2p":
+                        parcedData[i] = Convert.ToString(ParsingForFloat(feed[i], feed[i - 1]));   
+                        textBox2.AppendText(String.Format("Reg {0:000}-{1:000} |   Float\t| {2}\r\n", dataStringNumber[i-1], dataStringNumber[i], parcedData[i]));
+                        break;
+                    case "long2p":
+                        parcedData[i] = ParsingForLong(feed[i], feed[i - 1]);
+                        textBox2.AppendText(String.Format("Reg {0:000}-{1:000} |   Long\t| {2}\r\n", dataStringNumber[i-1], dataStringNumber[i], parcedData[i]));
+                        break;
+                    case "int":
+                        parcedData[i] = feed[i];
+                        textBox2.AppendText(String.Format("Reg {0:000}        |   Integer\t| {1}\r\n", dataStringNumber[i], parcedData[i]));
+                        break;
+                    case "int_reg92":
+                        parcedData[i] = ParsingForInt_reg92(feed[i]);
+                        textBox2.AppendText(String.Format("Reg {0:000}        | Integer 2 value\t| {1}\r\n", dataStringNumber[i], parcedData[i]));
+                        break;
+                    case "int_rng2048":
+                        parcedData[i] = ParsingForInt_rng2048(feed[i]);
+                        textBox2.AppendText(String.Format("Reg {0:000}        | Integer ranged\t| {1}\r\n", dataStringNumber[i], parcedData[i]));
+                        break;
+                    case "bit":
+                        parcedData[i] = ParsingForBit(feed[i]);
+                        textBox2.AppendText(String.Format("Reg {0:000}        |   Bit\t\t| {1}\r\n", dataStringNumber[i], parcedData[i]));
+                        break;
+                    case "bcd":
+                        parcedData[i] = ParsingForBCD(feed[i]);
+                        textBox2.AppendText(String.Format("Reg {0:000}        |   BCD\t| {1}\r\n", dataStringNumber[i], parcedData[i]));
+                        break;
+                    case "bcd2p":
+                        parcedData[i] = ParsingForBCD2reg(feed[i], feed[i - 1]);
+                        textBox2.AppendText(String.Format("Reg {0:000}-{1:000} |   BCD 2parts\t| {2}\r\n", dataStringNumber[i-1], dataStringNumber[i], parcedData[i]));
+                        break;
+                    case "bcd3pof3":
+                        parcedData[i] = ParsingForBCD3reg(feed[i], feed[i - 1], feed[i - 2]);
+                        textBox2.AppendText(String.Format("Reg {0:000}-{1:000} |   BCD 3parts\t| {2}\r\n", dataStringNumber[i-2], dataStringNumber[i], parcedData[i].Insert(6, " ").Insert(4, ":").Insert(2, ":")));
+                        break;
+                    default:
+                        parcedData[i] = "none";
+                        break;
+                }
+            }
+        }
+
+        static int REG_LENGTH = 16;
+        static int RANGE_0to2048_CONST = 32768;
+        public static float BinStringToSingle(string s)
+        {
+            int i = Convert.ToInt32(s, 2);
+            byte[] b = BitConverter.GetBytes(i);
+            return BitConverter.ToSingle(b, 0);
+        }
+        public static string BinStringToIntToString(string s)
+        {
+
+            return Convert.ToString(Convert.ToInt16(s, 2), 10);
+        }
+
+        public static float ParsingForFloat(string a, string b)
+        {
+            a = Convert.ToString(Convert.ToInt32(a), 2);
+            b = Convert.ToString(Convert.ToInt32(b), 2);
+            while (a.Length < REG_LENGTH)
+            {
+                a = '0' + a;
+            }
+            while (b.Length < REG_LENGTH)
+            {
+                b = '0' + b;
+            }
+            return BinStringToSingle(a + b);
+        }
+        public static string ParsingForLong(string a, string b)
+        {
+            a = Convert.ToString(Convert.ToInt32(a), 2);
+            b = Convert.ToString(Convert.ToInt32(b), 2);
+            while (a.Length < REG_LENGTH)
+            {
+                a = '0' + a;
+            }
+            while (b.Length < REG_LENGTH)
+            {
+                b = '0' + b;
+            }
+            return Convert.ToString(Convert.ToInt32(a + b, 2), 10);
+        }
+        public static string ParsingForInt_reg92(string a)
+        {
+            a = ParsingForBit(a);
+            a = BinStringToIntToString(a.Substring(0, 8)) + ' ' + BinStringToIntToString(a.Substring(8, 8));
+            return a;
+        }
+        public static string ParsingForInt_rng2048(string a)
+        {
+
+            int i = (int)Math.Floor(Convert.ToDecimal((Convert.ToInt32(a) + RANGE_0to2048_CONST) / 32));
+            a = Convert.ToString(i);
+            return a;
+        }
+        public static string ParsingForBit(string a)
+        {
+            a = Convert.ToString(Convert.ToInt16(a), 2);
+            while (a.Length < REG_LENGTH)
+            {
+                a = '0' + a;
+            }
+            return a;
+        }
+        public static string ParsingForBCD(string a)
+        {
+            string[] BCD = new string[4];
+            a = ParsingForBit(a);
+            for (int i = 0; i < 4; i++)
+            {
+                BCD[i] = BinStringToIntToString(a.Substring(i * 4, 4));
+            }
+            return String.Join("", BCD);
+        }
+        public static string ParsingForBCD2reg(string a, string b)
+        {
+            string[] BCD = new string[8];
+            a = ParsingForBit(a);
+            b = ParsingForBit(b);
+            for (int i = 0; i < 4; i++)
+            {
+                BCD[i] = BinStringToIntToString(a.Substring(i * 4, 4));
+                BCD[i + 4] = BinStringToIntToString(b.Substring(i * 4, 4));
+            }
+            return String.Join("", BCD);
+        }
+        public static string ParsingForBCD3reg(string a, string b, string c)
+        {
+
+            string[] BCD = new string[12];
+            a = ParsingForBit(a);
+            b = ParsingForBit(b);
+            c = ParsingForBit(c);
+            for (int i = 0; i < 4; i++)
+            {
+                BCD[i] = BinStringToIntToString(a.Substring(i * 4, 4));
+                BCD[i + 4] = BinStringToIntToString(b.Substring(i * 4, 4));
+                BCD[i + 8] = BinStringToIntToString(c.Substring(i * 4, 4));
+            }
+            return String.Join("", BCD); ;
+        }
+
+        private void Save_Text_Click(object sender, EventArgs e)
+        {
+            
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName);
+                streamWriter.WriteLine(textBox2.Text);
+                streamWriter.Close();
+            }
+
+        }
+    }
+
+}
